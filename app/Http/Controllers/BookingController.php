36b89aa51d707514ca67fc22138402c43f6c5eac
Arisiki <arisiki123@gmail.com>
2025-05-21 10:09:28 +0800

    Fix key midtrans for production

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -250,9 +250,9 @@ class BookingController extends Controller
 
         // Payment Gateway (Midtrans)
         \Midtrans\Config::$serverKey = config('midtrans.serverKey');
-        \Midtrans\Config::$isProduction = false;
-        \Midtrans\Config::$isSanitized = true;
-        \Midtrans\Config::$is3ds = true;
+        \Midtrans\Config::$isProduction = config('midtrans.isProduction');
+        \Midtrans\Config::$isSanitized = config('midtrans.isSanitized');
+        \Midtrans\Config::$is3ds = config('midtrans.is3ds');
 
         $params = array(
             'transaction_details' => array(
